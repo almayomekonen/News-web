f981203bac21186b9e427a9d20d25aa55fe2d906
alamayomekonen <alamayo274@gmail.com>
2023-09-06 15:39:54 -0700

    adding header to the news

--- src/news/Card.js
+++ src/news/Card.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const Card = ({ article }) => (
   <Link to={`/news/${article.id}`}>
     <div className="Card">
+      <header>{article.headline}</header>
       <div
         className="card-img"
         style={{ backgroundImage: `url('${article.imgUrl}')` }}
       ></div>
-      <header>{article.headline}</header>
       <footer>{article.description}</footer>
     </div>
   </Link>
